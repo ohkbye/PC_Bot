e2765d6164cbed48a6de50a501bfff863312d60b
Karlos <karlosc029@gmail.com>
2022-03-29 12:22:30 +1300

    Added comments for each section
    Comments for each section

---
+++ pc_bot.py
@@ -0,0 +1,25 @@
+# PC bot program
+
+# Welcome message with random name
+
+# Menu for click & collect or delivery
+
+# Click & collect information - name and phone number
+
+# Delivery informtion - name, address and phone
+
+# Choose type of PC component
+
+# PC component menu
+
+# PC component order - from menu - print each component ordered with cost
+
+# Print order out - including if order is delivery or click & collect and names and price of each component - total cost including any delivery charge
+
+# Ability to remove an item, cancel or proceed with order
+
+# Option for new order or to exit
+
+# Payment method
+
+# Main function 
