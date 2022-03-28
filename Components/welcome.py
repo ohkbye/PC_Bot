bbdf1a856f21fad8d8345ff9f59b6e1bcf588726
Karlos <karlosc029@gmail.com>
2022-03-29 12:09:03 +1300

    Updated welcome function
    Added code to the welcome.py file

---
+++ Components/welcome.py
@@ -0,0 +1,21 @@
+# importing randint from random module
+from random import randint
+
+# List of random names
+names = ["Karlos", "Marcus", "Jaiden", "Railey", "Ivan", "James", "Joaquin", "Einstein", "Mikara", "Sharun", 
+"Tomas", "Matt", "Levi", "Dennis", "Ochre", "Carlos", "Navhil", "George", "Billy", "Noel"]
+
+# Creating an integer variable, num with a random value from 0 to 9
+num = randint(0, 9)
+
+# Picking a random name from the names list and making it a string
+name_msg = "My name is " + names[num]
+
+# Print welcome message
+welcome_msg = ["Welcome to Joaquins PC Store", name_msg, "I will be here to help you order your PC component"]
+print("\n\n\n")
+# Center text
+for x in range(len(welcome_msg)):
+    print(str("***** " + welcome_msg[x] + " *****").center(200," "))
+print("\n\n\n")
+
