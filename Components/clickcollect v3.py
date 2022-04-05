8fe1ff5ab607b08bc10718ccce7e28c302a75914
Karlos <karlosc029@gmail.com>
2022-04-06 09:41:07 +1200

    Created click & collect v3 and v4
    Created a function for not_blank and used it to validate name and phone. Added a customer details dictionary to store customer details name and phone

---
+++ Components/clickcollect v3.py
@@ -0,0 +1,27 @@
+# Customer details dictionary
+customer_details = {}
+
+# Basic instruction
+print("Please enter the click & collect information")
+
+# Customer name not blank
+valid = False
+while not valid:
+    customer_details['name'] = input("Please enter your name: ")
+    if customer_details['name'] != "":
+        print(customer_details['name'])
+        break
+    else:
+        print("Sorry this cannot be blank")
+
+
+
+# Customer phone number not blank
+valid = False
+while not valid:
+    customer_details['phone'] = input("Please enter your phone number: ")
+    if customer_details['phone'] != "":
+        print(customer_details['phone'])
+        break
+    else:
+        print("Sorry this cannot be blank")
