3cd7dd665894a93d01d8a2026fe333a9db215a24
Karlos <karlosc029@gmail.com>
2022-04-06 09:17:34 +1200

    Created version 1 of Click & collect function
    Basic instructions and input for name and phone number

---
+++ Components/clickcollect v1.py
@@ -0,0 +1,9 @@
+print("Please enter the click & collect information")
+
+# Customer name
+name = input("Please enter a name: ")
+
+# Customer phone number
+phone = input("Please enter your phone number")
+
+print(name, phone)
